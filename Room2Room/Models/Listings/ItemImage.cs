176fe6e22f4afc57122487c58b71ccfe1ba78b82
semaphoreError <pkshetri34@gmail.com>
2026-02-19 19:29:34 -0600

    added item and item images relationship on the listings model

---
+++ Room2Room/Models/Listings/ItemImage.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+namespace Room2Room.Models.Listings;
+public class ItemImage
+{
+    public int Id { get; set; }
+    [Required]
+    public string ImagePath { get; set; }
+    [Required]
+    public int ItemId { get; set; } // foreign key to Items
+    public Items Item { get; set; } // many to one relationship with Items
+}
--- Room2Room/Models/Listings/Items.cs
+++ Room2Room/Models/Listings/Items.cs
@@ -13,10 +13,9 @@ public class Items
     [Required]
     public string Status { get; set; } = "Available";
     [Required]
-    public string Image { get; set; }
-    [Required]
-    public int CategoryId { get; set; } // one item can only have one category
+    public int CategoryId { get; set; }
     public Category Category { get; set; }
     public List<OrderDetail> OrderDetail { get; set; }
     public List<CartDetail> CartDetail { get; set; }
+    public List<ItemImage> ItemImages { get; set; } // one to many relationship with ItemImage
 }
