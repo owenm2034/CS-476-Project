3f0472debd5de73372e170b7825b9157dd2a0d54
Owen Monus <owenmonus@icloud.com>
2026-03-01 17:15:08 -0600

    merge notification preferences into one object

--- Room2Room/Models/NotificationPreferences/AdministratorNotificationPreference.cs
+++
@@ -1,7 +0,0 @@
-namespace Room2Room.Models.NotificationPreferences;
-
-public class AdministratorNotificationPreference : NotificationPreference
-{
-    public bool RecieveEmailNotificationOnUserReported { get; set; } = true;
-    public bool RecieveEmailNotificationOnListingReported { get; set; } = true;
-}
--- Room2Room/Models/NotificationPreferences/NotificationPreference.cs
+++ Room2Room/Models/NotificationPreferences/NotificationPreference.cs
@@ -4,7 +4,16 @@ namespace Room2Room.Models.NotificationPreferences;
 
 public class NotificationPreference
 {
+    public NotificationPreference() {}
+
+    public NotificationPreference(int accountId)
+    {
+        AccountId = accountId;
+    }
+
     [Key]
     public int AccountId { get; set; }
     public bool RecieveEmailNotificationOnChatMessageRecieved { get; set; } = true;
+    public bool RecieveEmailNotificationOnUserReported { get; set; } = true;
+    public bool RecieveEmailNotificationOnListingReported { get; set; } = true;
 }
