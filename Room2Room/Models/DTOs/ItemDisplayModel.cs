a0e0bdcdfb1b4f7f174dd26b9e24f473c2e9a1e5
Owen Monus <76464774+owenm2034@users.noreply.github.com>
2026-03-01 20:28:57 -0600

    Allow admins to search & filter listings (#45)

--- Room2Room/Models/DTOs/ItemDisplayModel.cs
+++ Room2Room/Models/DTOs/ItemDisplayModel.cs
@@ -4,9 +4,11 @@ namespace Room2Room.Models.DTOs
     {
         public IEnumerable<Item> Items { get; set; }
         public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<University> Universities { get; set; }
         
         public string STerm { get; set; } = "";
-        public int CategoryId { get; set; } = 0;
+        public int? CategoryId { get; set; }
+        public int? UniversityId { get; set; }
     }
 
 }
