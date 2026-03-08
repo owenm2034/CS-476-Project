3bc1a19535a7c6f6481ea22735b9cae9349b2b81
Owen Monus <owenmonus@icloud.com>
2026-03-07 18:54:46 -0600

    chats looking good

--- Room2Room/Controllers/ChatController.cs
+++ Room2Room/Controllers/ChatController.cs
@@ -64,4 +64,39 @@ public class ChatController : Controller
 
         return PartialView("_Chats", chatModels);
     }
+
+    
+    [HttpPost]
+    public IActionResult SendMessage(ChatMessage message) {
+        // todo
+        var nextId = _context.ChatMessage.OrderByDescending(x => x.MessageId).First().MessageId + 1;
+        
+        message.FromAccountId = int.Parse(User.Claims.FirstOrDefault(c => c.Type == "AccountId")?.Value ?? "0");
+        message.CreatedAt = DateTime.Now;
+        message.MessageId = nextId;
+        _context.Add(message);
+        _context.SaveChanges();
+        
+        var chat = _context.Chat.Where(x => x.ChatId == message.ChatId).ToList().First();
+        var messagesTask = _context.ChatMessage.Where(x => x.ChatId == message.ChatId).ToList();
+        var accountIdsTask = _context.ChatMember.Where(x => x.ChatId == message.ChatId).Select(x => x.AccountId).ToList();
+
+        Dictionary<int, Item> itemIdToItemDict =
+            _context.Items.Where(x => x.Id == chat.ListingId)
+                .ToList().Distinct().ToDictionary(x => x.Id);
+
+        List<Account> accounts = _context.Accounts.Where(x => accountIdsTask.Distinct().ToList().Contains(x.Id)).ToList();
+
+        var accountIds = messagesTask.Where(x => x.ChatId == chat.ChatId).Select(x => x.FromAccountId).ToList().Distinct().ToList();
+        var accountDict = accounts.Where(x => accountIds.Contains(x.Id)).ToDictionary(x => x.Id, x => x.Username);
+
+        var cm = new ChatModel {
+            Chat = chat,
+            Item = chat.ListingId.HasValue ? itemIdToItemDict.GetValueOrDefault(chat.ListingId.Value) : null,
+            AccountIdToNameDictionary = accountDict,
+            Messages = messagesTask.Where(x => x.ChatId == chat.ChatId).OrderBy(x => x.CreatedAt).ToList()
+        };
+
+        return PartialView("_Messages", cm);
+    }
 }
