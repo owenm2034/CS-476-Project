204d04b519fe593629dd6897464f3cce64b3c393
coleyager <109551749+coleyager@users.noreply.github.com>
2026-03-10 17:23:51 -0600

    Added user list to 'Users' tab under Admin controls

---
+++ Room2Room/Models/DTOs/AdminUsersViewModels.cs
@@ -0,0 +1,24 @@
+namespace Room2Room.Models.DTOs;
+
+public class AdminUsersViewModel
+{
+    public IEnumerable<AdminUserListItem> Users { get; set; } = new List<AdminUserListItem>();
+    public string STerm { get; set; } = string.Empty;
+}
+
+public class AdminUserListItem
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public bool IsAdmin { get; set; }
+    public string UniversityName { get; set; } = string.Empty;
+}
+
+public class AdminEditUserModel
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public bool IsAdmin { get; set; }
+}
