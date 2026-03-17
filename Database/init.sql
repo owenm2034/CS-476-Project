05e86a9a350687245d085bdf7ec02016bba975b4
semaphoreError <pkshetri34@gmail.com>
2026-03-17 17:39:42 -0600

    added table UserNotification to represent the notification issued to user, created model, registered in ApplicationDbContext, build verified, everything looks good so far

--- Database/init.sql
+++ Database/init.sql
@@ -192,3 +192,21 @@ print ('ChatMember created')
 END
 GO
 
+IF NOT EXISTS (select * from sys.tables where name = 'UserNotification')
+BEGIN
+CREATE TABLE UserNotification(
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    UserId INT NOT NULL,
+    ItemId INT NOT NULL,
+    Message NVARCHAR(500) NOT NULL,
+    Type NVARCHAR(50) NOT NULL,
+    IsRead BIT NOT NULL DEFAULT 0,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+
+    CONSTRAINT FK_UserNotification_Accounts_UserId FOREIGN KEY (UserId) REFERENCES Accounts(Id),
+    CONSTRAINT FK_UserNotification_Item_ItemId FOREIGN KEY (ItemId) REFERENCES Item(Id)
+);
+print ('UserNotification created')
+END
+ELSE
+    PRINT('UserNotification already created :)');
