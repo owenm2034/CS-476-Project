ac330a98972bbb890a1f814e6c5204606317dc7c
Owen Monus <owenmonus@icloud.com>
2026-03-26 19:30:02 -0600

    refactor Observer fnc

--- Room2Room/Services/Observers/IItemSubject.cs
+++ Room2Room/Services/Observers/IItemSubject.cs
@@ -2,8 +2,8 @@ namespace Room2Room.Services.Observers
 {
     public interface IItemSubject
     {
-        void Attach(IItemObserver observer);
-        void Detach(IItemObserver observer);
+        void Register(IItemObserver observer);
+        void Unregister(IItemObserver observer);
         void Notify();
         void UpdateItem(Item oldItem, Item newItem);
     }
