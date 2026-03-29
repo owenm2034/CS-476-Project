24b803dcebe3076d1566eef1656c2d64252f4f5c
Owen Monus <owenmonus@icloud.com>
2026-03-29 12:39:39 -0600

    Fix incorrect type match on listing creation

--- Room2Room/Models/DTOs/CreateItem.cs
+++ Room2Room/Models/DTOs/CreateItem.cs
@@ -6,7 +6,7 @@ namespace Room2Room.Models.DTOs
     {
         public string ItemName { get; set; }
         public string ItemDescription { get; set; }
-        [Range(typeof(decimal), "0", "10000")]
+        [Range(typeof(double), "0", "10000")]
         public double Price { get; set; }
         public int CategoryId { get; set; }
 
